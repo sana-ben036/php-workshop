e7c9440ed77a91bb546f8e8329ca22051298c9a7
SANA BEN <sana.bengannoune@gmail.com>
2020-06-01 01:59:30 +0100

    Update activity-movies.php

--- e3/activity-movies.php
+++ e3/activity-movies.php
@@ -1,17 +1,17 @@
 <?php
 
-
+$realisateurs = [
+    'realisateur1'=> ['Titre11','Titre12','Titre13','Titre14','Titre15'],
+    'realisateur2'=> ['Titre21','Titre22','Titre23','Titre24','Titre25'],
+    'realisateur3'=> ['Titre31','Titre32','Titre33','Titre34','Titre35'],
+    'realisateur4'=> ['Titre41','Titre42','Titre43','Titre44','Titre45'],
+    'realisateur5'=> ['Titre51','Titre52','Titre53','Titre54','Titre55']
+    
+    ];
 
 function showDetails (int $r=5, int $t=5){
 
-    $realisateurs = [
-        'realisateur1'=> ['Titre11','Titre12','Titre13','Titre14','Titre15'],
-        'realisateur2'=> ['Titre21','Titre22','Titre23','Titre24','Titre25'],
-        'realisateur3'=> ['Titre31','Titre32','Titre33','Titre34','Titre35'],
-        'realisateur4'=> ['Titre41','Titre42','Titre43','Titre44','Titre45'],
-        'realisateur5'=> ['Titre51','Titre52','Titre53','Titre54','Titre55']
-        
-        ];
+    global $realisateurs;                  // permettre de connaitre un variable declaré hors de la fonction
 
     $nbrR = array_rand($realisateurs, $r); //pour determiner le nombre des realisateurs à afficher (note :array_rand() expects parameter 1 to be array)
     $nbrT = array_rand($nbrR, $t);        // pour determiner le nombre de titre à afficher (note: Second argument has to be between 1 and the number of elements in the array)
