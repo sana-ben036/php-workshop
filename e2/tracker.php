83a891094a3d837b213ecef219517404a5535194
SANA BEN <sana.bengannoune@gmail.com>
2020-06-01 01:21:17 +0100

    Update tracker.php

--- e2/tracker.php
+++ e2/tracker.php
@@ -6,7 +6,7 @@ $name = "SANA" ;
 $weightKg = 48;
 $heightCm = 158 ;
 $heightM = $heightCm/100;
-$heightM2 = $heightM * $heightM;
+$heightM2 = $heightM **2;
 
 $IMC = $weightKg / $heightM2;
 
@@ -18,7 +18,7 @@ echo "Bonjour $name , votre IMC est $IMC";
 
 function CalculerImc($name,$weightKg,$heightCm){ // fonction pour calculer IMC d'un utilisateur
 
-    $IMC = $weightKg / ($heightCm*$heightCm/10000);
+$IMC = $weightKg / ($heightCm/100)**2;
     $message = "Bonjour $name , votre IMC est $IMC";
     return $message;
 
