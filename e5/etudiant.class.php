f3eeab4e560a971e367f188d5025b3e4c2124cb1
SANA BEN <sana.bengannoune@gmail.com>
2020-06-13 17:28:00 +0100

    creation des classes e5

---
+++ e5/etudiant.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class Etudiant{
+    private $nom;
+    private $titre = 'Student';
+
+
+    public function __construct($n){
+        $this->nom = $n;
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+?>
---
+++ e5/professeur.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Professeur{
+    private $nom;
+    private $titre = 'Prof';
+    private $list_etudiants;
+
+
+    public function __construct($n,$l){
+        $this->nom = $n;
+        $this->list_etudiants = $l;
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+?>
